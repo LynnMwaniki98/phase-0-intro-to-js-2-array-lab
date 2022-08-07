f579fe5d373aebf7213008982f87be8891205dc0
Lynn Mwaniki <Lynn.mwaniki@student.moringaschool.com>
2022-08-07 20:06:05 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Write your solution here!
+let cats= ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(){
+    cats.push("Ralph")
+}
+        
+function destructivelyPrependCat(){
+    cats.unshift("Bob")
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop("Garfield")
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift("Milo")
+}
+
+function appendCat(){
+let ginger = cats.concat(["Broom"])
+    return ginger
+}
+
+function prependCat(){
+const copyOfCats = [...cats]
+copyOfCats.unshift("Arnold")
+    return copyOfCats
+}
+
+function removeLastCat(){
+    const newCopy = [...cats]
+   console.log(newCopy.pop())
+   return newCopy
+}
+ function removeFirstCat(){
+   const lastCopy = [...cats] 
+   console.log(lastCopy.shift())
+   return lastCopy
+}
